0bee989849be4756b76f8be22165abb457964e69
EliptickBlock5 <nbjohnson@gmail.com>
2021-06-25 12:23:30 -0700

    server listen code

--- server.js
+++ server.js
@@ -10,3 +10,7 @@ const PORT = process.env.PORT || 8800;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: ture }));
 app.use(express.json());
+
+
+//listening to server
+app.listen(PORT, () => console.log("Nate your port is on" + PORT));

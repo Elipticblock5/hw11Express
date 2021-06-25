8e09eeb39ab054643f50a3985227547b939ca7ca
EliptickBlock5 <nbjohnson@gmail.com>
2021-06-25 12:34:40 -0700

    notes code, not working

--- server.js
+++ server.js
@@ -21,5 +21,22 @@ app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
+
+//new note code POST route
+app.post("/api/notes", (req, res) => {
+    let createNote = req.body;
+    let notesList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let notesLength = (notesList.length).toString();
+
+    //id for new note
+    createNote.id = notelenght;
+    notesList.push(createNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesList));
+    res.json(notesList);
+})
+
+
+
 //listening to server
 app.listen(PORT, () => console.log("Nate your port is on" + PORT));
